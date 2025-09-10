e8fe21827af874fef706d6e034ad703e885b0496
jmasec <jmasecuser@gmail.com>
2025-09-09 22:10:50 -0400

    cont on net, untested, defining approach of parsing commands from the TCP stream

--- kvd-net/src/lib.rs
+++ kvd-net/src/lib.rs
@@ -19,6 +19,21 @@ use std::sync::Arc;
 use std::sync::RwLock;
 use std::{io::BufReader, net::TcpStream};
 
+enum RespType {
+    SimpleStrings(Option<WaitingCommands<String>>),
+    SimpleErrors(Option<WaitingCommands<String>>),
+    Integers(Option<WaitingCommands<u64>>),
+    BulkStrings(Option<WaitingCommands<String>>),
+    Arrays(Option<WaitingCommands<Vec<String>>>),
+    Nulls(Option<WaitingCommands<char>>),
+    Booleans(Option<WaitingCommands<char>>),
+}
+
+struct WaitingCommands<T> {
+    done: bool,
+    data: T,
+}
+
 enum Commands {
     Ping,
     Echo(String),
@@ -31,11 +46,6 @@ enum Commands {
     Persist(String),
     Info,
     Error(String),
-}
-
-enum FirstByte {
-    SimpleStrings(u8),
-    SimepleErrors(u8),
 }
 
 impl Commands {
@@ -57,24 +67,59 @@ impl Commands {
     }
 }
 
+struct CommandsQueue {
+    queue: Vec<RespType>,
+    commands_dropped: u64,
+}
+
 fn handle_connections(stream: TcpStream) -> std::io::Result<()> {
     let perr_addr: std::net::SocketAddr = stream.peer_addr()?;
     println!("Client {:?} connected", perr_addr);
 
     let buf_reader: BufReader<TcpStream> = BufReader::new(stream);
 
+    let mut command_queue: CommandsQueue = CommandsQueue {
+        commands_dropped: 0,
+        queue: Vec::new(),
+    };
+
     for line in buf_reader.lines() {
         let line: String = line?;
         // parse it out into a commands
+        // ok so things to think about for edge cases
+        // array would need to keep grabbing multiple loops
+        // other types take two loops to grab their data
+        // if we hit another start byte and we are not working with an array
+        // this would be a new command we hit now, so they were back to back
+        let byte_stream: &[u8] = line.as_bytes();
+        let first_byte: u8 = byte_stream[0];
+        let data_type: Option<RespType> = find_resp_type(first_byte);
+        if data_type.is_none() {
+            // send back an error code for not valid type
+            break;
+        }
     }
 
     Ok(())
 }
 
-// fn recv_bytes() {}
+fn find_resp_type(first_byte: u8) -> Option<RespType> {
+    match first_byte {
+        47 => Some(RespType::SimpleStrings(None)),
+        45 => Some(RespType::SimpleErrors(None)),
+        58 => Some(RespType::Integers(None)),
+        36 => Some(RespType::BulkStrings(None)),
+        42 => Some(RespType::Arrays(None)),
+        95 => Some(RespType::Nulls(None)),
+        35 => Some(RespType::Booleans(None)),
+        _ => None,
+    }
+}
 
-fn parse_resp(bytes: Vec<u8>) -> String {
-    "string".to_string()
+fn parse_resp(bytes: &[u8], resp_type: RespType) -> Vec<&str> {
+    match resp_type {
+        _ => vec![""],
+    }
 }
 
 fn main() {
